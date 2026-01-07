94014f9b5a34601596b50329142ba9fe2178234f
AndresHerrera <t763rm3n@gmail.com>
2026-01-06 22:51:36 -0500

    update to version 0.2

--- com/map/ServiceUrlMap.py
+++ com/map/ServiceUrlMap.py
@@ -1,5 +1,5 @@
 from ..enum.Service import Service
 
 service_url_map = {
-    Service.IDESCaliDataset.value: 'http://ws-idesc.cali.gov.co:8081/geoserver/wms?service=WMS&version=1.1.0'
+    Service.IDESCaliDataset.value: 'http://ws-idesc.cali.gov.co/geoserver/wms?service=WMS&version=1.1.0'
 }
--- idescali_ws.py
+++ idescali_ws.py
@@ -20,9 +20,9 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, pyqtSignal, QEvent
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, pyqtSignal, QEvent, Qt
 from qgis.PyQt.QtGui import QIcon, QTextCursor
-from qgis.PyQt.QtWidgets import QAction, QMessageBox, QTableWidgetItem, QHeaderView, QAbstractItemView, QProgressBar, QProgressDialog
+from qgis.PyQt.QtWidgets import QAction, QMessageBox, QTableWidgetItem, QHeaderView, QAbstractItemView, QProgressBar, QProgressDialog, QCheckBox, QWidget, QHBoxLayout, QComboBox
 from qgis._core import Qgis, QgsRasterLayer, QgsProject,QgsMessageLog
 from .info_dialog import InfoDialog
 from .com.map.ServiceUrlMap import service_url_map
@@ -63,6 +63,8 @@ class IDESCaliWebServices:
         self.actions = []
         self.menu = self.tr(u'&Servicios WMS - Geoportal IDESC')
         self.first_start = None
+        self.all_contents = OrderedDict()  # Almacenar todas las capas sin filtrar
+        self.selected_layers = set()  # Almacenar las capas seleccionadas (por nombre)
 
     def tr(self, message):
         return QCoreApplication.translate('IDESCaliWebServices', message)
@@ -108,7 +110,9 @@ class IDESCaliWebServices:
         self.dlg.help_button.clicked.connect(self.openDlgInfo)
         self.dlg.close_button.clicked.connect(self.closeDlg)
         self.dlg.search_box.textEdited.connect(self.search)
+        self.dlg.workspace_combo.currentTextChanged.connect(self.filterByWorkspace)
         self.dlg.add_button.released.connect(self.loadWebService)
+        self.dlg.clear_button.released.connect(self.clearSelectedLayers)
         self.dlginfo.ok_dialog.released.connect(self.closeAbout)
         self.first_start = True
 
@@ -123,6 +127,7 @@ class IDESCaliWebServices:
     def run(self):
         if self.generatedService.web_map_service is not None:
             self.dlg.search_box.clear()
+            self.dlg.workspace_combo.setCurrentIndex(0)  # Resetear a "Todos"
             self.fill_table(self.generatedService.web_map_service.contents)
             self.dlg.show()
             result = self.dlg.exec_()
@@ -169,6 +174,7 @@ class IDESCaliWebServices:
         self.dlg.table_widget.setRowCount(0)
         self.dlg.table_widget.setColumnCount(0)
         self.dlg.layer_name_box.clear()
+        self.selected_layers.clear()  # Limpiar selecciones al cerrar
         self.dlg.close()
         if self.dlginfo:
             self.dlginfo.close()
@@ -177,10 +183,39 @@ class IDESCaliWebServices:
         if self.dlginfo:
             self.dlginfo.close()
 
+    def extract_workspace(self, layer_name):
+        """Extrae el espacio de trabajo del nombre de la capa (formato: espaciotrabajo:nombre capa)"""
+        if ':' in layer_name:
+            return layer_name.split(':')[0]
+        return ""
+
+    def get_unique_workspaces(self, contentOrderedDict):
+        """Obtiene los espacios de trabajo únicos de las capas"""
+        workspaces = set()
+        for content in contentOrderedDict:
+            name = contentOrderedDict[content].name
+            workspace = self.extract_workspace(name)
+            if workspace:
+                workspaces.add(workspace)
+        return sorted(list(workspaces))
+
     def fill_table(self, contentOrderedDict):
+        # Guardar todas las capas para filtrado
+        self.all_contents = contentOrderedDict
+        
+        # Extraer y poblar espacios de trabajo únicos
+        workspaces = self.get_unique_workspaces(contentOrderedDict)
+        self.dlg.workspace_combo.blockSignals(True)  # Bloquear señales para evitar filtrado durante la carga
+        self.dlg.workspace_combo.clear()
+        self.dlg.workspace_combo.addItem("Todos")  # Opción para mostrar todos
+        for workspace in workspaces:
+            self.dlg.workspace_combo.addItem(workspace)
+        self.dlg.workspace_combo.blockSignals(False)
+        
+        # Llenar la tabla
         self.dlg.table_widget.setRowCount(0)
         count = self.dlg.table_widget.rowCount()
-        self.dlg.table_widget.setColumnCount(4)
+        self.dlg.table_widget.setColumnCount(5)
 
         for content in contentOrderedDict:
             index = count
@@ -188,19 +223,42 @@ class IDESCaliWebServices:
             title = contentOrderedDict[content].title
             abstract = contentOrderedDict[content].abstract
             self.dlg.table_widget.insertRow(index)
-            self.dlg.table_widget.setItem(index, 1, QTableWidgetItem(str(name)))
-            self.dlg.table_widget.setItem(index, 2, QTableWidgetItem(str(title)))
-            self.dlg.table_widget.setItem(index, 3, QTableWidgetItem(str(abstract)))
+            
+            # Columna 0: Checkbox
+            checkbox = QCheckBox()
+            # Restaurar el estado del checkbox si la capa estaba seleccionada
+            if name in self.selected_layers:
+                checkbox.setChecked(True)
+            checkbox.stateChanged.connect(self.updateSelectedLayers)
+            checkbox_widget = QWidget()
+            checkbox_layout = QHBoxLayout(checkbox_widget)
+            checkbox_layout.addWidget(checkbox)
+            checkbox_layout.setAlignment(Qt.AlignCenter)
+            checkbox_layout.setContentsMargins(0, 0, 0, 0)
+            self.dlg.table_widget.setCellWidget(index, 0, checkbox_widget)
+            
+            # Columna 1: ID (oculta)
+            self.dlg.table_widget.setItem(index, 1, QTableWidgetItem(str(content)))
+            # Columna 2: Capa
+            self.dlg.table_widget.setItem(index, 2, QTableWidgetItem(str(name)))
+            # Columna 3: Nombre
+            self.dlg.table_widget.setItem(index, 3, QTableWidgetItem(str(title)))
+            # Columna 4: Resumen
+            self.dlg.table_widget.setItem(index, 4, QTableWidgetItem(str(abstract)))
 
-        self.dlg.table_widget.setHorizontalHeaderLabels(["ID", "Capa", "Nombre", "Resumen"])
+        self.dlg.table_widget.setHorizontalHeaderLabels(["", "ID", "Capa", "Nombre", "Resumen"])
         self.dlg.label_conteo.setText("Capas disponibles: "+str(len(contentOrderedDict)))
         self.setTableWidgetBehaviour()
+        
+        # Actualizar la lista de capas seleccionadas después de restaurar
+        self.updateSelectedLayers()
 
     def setTableWidgetBehaviour(self):
-        self.dlg.table_widget.setColumnWidth(0, 0)
-        self.dlg.table_widget.setColumnWidth(1, 200)
-        self.dlg.table_widget.setColumnWidth(2, 200)
-        self.dlg.table_widget.setColumnWidth(3, 200)
+        self.dlg.table_widget.setColumnWidth(0, 50)
+        self.dlg.table_widget.setColumnWidth(1, 0)
+        self.dlg.table_widget.setColumnWidth(2, 180)
+        self.dlg.table_widget.setColumnWidth(3, 180)
+        self.dlg.table_widget.setColumnWidth(4, 180)
         self.dlg.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
         self.dlg.table_widget.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
 
@@ -208,25 +266,93 @@ class IDESCaliWebServices:
         self.dlg.table_widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.dlg.table_widget.setSelectionMode(QAbstractItemView.ExtendedSelection)
 
-    def search(self):
-        criteria = self.dlg.search_box.text()
-        criteria = criteria.lower()
+    def filterByWorkspace(self):
+        """Filtra las capas por espacio de trabajo seleccionado"""
+        self.applyFilters()
+
+    def applyFilters(self):
+        """Aplica los filtros de espacio de trabajo y búsqueda por nombre"""
+        # Actualizar las selecciones actuales antes de filtrar (sin borrar las que no están visibles)
+        self._updateSelectedLayersFromTable()
+        
+        if not hasattr(self, 'all_contents') or not self.all_contents:
+            # Si no hay contenido guardado, usar el contenido original
+            if self.generatedService and self.generatedService.web_map_service:
+                contents = self.generatedService.web_map_service.contents
+            else:
+                return
+        else:
+            contents = self.all_contents
+        
+        # Obtener filtros
+        selected_workspace = self.dlg.workspace_combo.currentText()
+        search_criteria = self.dlg.search_box.text().lower()
+        
         wms_filtered_contents = OrderedDict()
-        contents = self.generatedService.web_map_service.contents
         for content in contents:
             name = contents[content].name
-            if criteria in name:
+            workspace = self.extract_workspace(name)
+            
+            # Filtrar por espacio de trabajo
+            workspace_match = (selected_workspace == "Todos" or workspace == selected_workspace)
+            
+            # Filtrar por búsqueda de nombre
+            name_match = (not search_criteria or search_criteria in name.lower())
+            
+            if workspace_match and name_match:
                 wms_filtered_contents[content] = contents[content]
-        self.fill_table(wms_filtered_contents)
+        
+        # Actualizar tabla sin repoblar el ComboBox
+        self.dlg.table_widget.setRowCount(0)
+        count = 0
+        self.dlg.table_widget.setColumnCount(5)
+
+        for content in wms_filtered_contents:
+            index = count
+            name = wms_filtered_contents[content].name
+            title = wms_filtered_contents[content].title
+            abstract = wms_filtered_contents[content].abstract
+            self.dlg.table_widget.insertRow(index)
+            
+            # Columna 0: Checkbox
+            checkbox = QCheckBox()
+            # Restaurar el estado del checkbox si la capa estaba seleccionada
+            if name in self.selected_layers:
+                checkbox.setChecked(True)
+            checkbox.stateChanged.connect(self.updateSelectedLayers)
+            checkbox_widget = QWidget()
+            checkbox_layout = QHBoxLayout(checkbox_widget)
+            checkbox_layout.addWidget(checkbox)
+            checkbox_layout.setAlignment(Qt.AlignCenter)
+            checkbox_layout.setContentsMargins(0, 0, 0, 0)
+            self.dlg.table_widget.setCellWidget(index, 0, checkbox_widget)
+            
+            # Columna 1: ID (oculta)
+            self.dlg.table_widget.setItem(index, 1, QTableWidgetItem(str(content)))
+            # Columna 2: Capa
+            self.dlg.table_widget.setItem(index, 2, QTableWidgetItem(str(name)))
+            # Columna 3: Nombre
+            self.dlg.table_widget.setItem(index, 3, QTableWidgetItem(str(title)))
+            # Columna 4: Resumen
+            self.dlg.table_widget.setItem(index, 4, QTableWidgetItem(str(abstract)))
+            
+            count += 1
+
+        self.dlg.table_widget.setHorizontalHeaderLabels(["", "ID", "Capa", "Nombre", "Resumen"])
+        self.dlg.label_conteo.setText("Capas disponibles: "+str(len(wms_filtered_contents)))
+        self.setTableWidgetBehaviour()
+        
+        # Actualizar la lista de capas seleccionadas después de restaurar
+        self.updateSelectedLayers()
+
+    def search(self):
+        """Filtra las capas por nombre"""
+        self.applyFilters()
 
     def getSelectedItemsFromTable(self):
-        rowNames = []
-        selected = self.dlg.table_widget.selectedItems()
-        if len(selected) > 0:
-            for i in range(0, len(selected), 4):
-                row = self.dlg.table_widget.row(selected[i])
-                name = self.dlg.table_widget.item(row, 1).text()
-                rowNames.append(name)
+        # Usar self.selected_layers que contiene todas las capas seleccionadas,
+        # incluso las que no están visibles en el filtro actual
+        rowNames = list(self.selected_layers)
 
         selectedServices = OrderedDict()
         contents = self.generatedService.web_map_service.contents
@@ -251,6 +377,72 @@ class IDESCaliWebServices:
             self.dlg.layer_name_box.setReadOnly(True)
         except:
             QgsMessageLog.logMessage("No selecciono ninguna capa WMS para cargar")
+
+    def _updateSelectedLayersFromTable(self):
+        """Actualiza las selecciones basándose en la tabla actual, preservando las que no están visibles"""
+        # Obtener las capas visibles actualmente
+        visible_layers = set()
+        for row in range(self.dlg.table_widget.rowCount()):
+            checkbox_widget = self.dlg.table_widget.cellWidget(row, 0)
+            if checkbox_widget:
+                checkbox = checkbox_widget.findChild(QCheckBox)
+                if checkbox:
+                    name = self.dlg.table_widget.item(row, 2).text()
+                    visible_layers.add(name)
+                    if checkbox.isChecked():
+                        self.selected_layers.add(name)
+                    else:
+                        # Solo remover si está visible y desmarcada
+                        self.selected_layers.discard(name)
+        
+        # Mantener las capas seleccionadas que no están visibles (no las removemos)
+    
+    def updateSelectedLayers(self):
+        """Actualiza la lista de capas seleccionadas cuando cambia el estado de un checkbox"""
+        try:
+            # Actualizar solo las capas visibles en la tabla, preservando las que no están visibles
+            for row in range(self.dlg.table_widget.rowCount()):
+                checkbox_widget = self.dlg.table_widget.cellWidget(row, 0)
+                if checkbox_widget:
+                    checkbox = checkbox_widget.findChild(QCheckBox)
+                    if checkbox:
+                        name = self.dlg.table_widget.item(row, 2).text()
+                        if checkbox.isChecked():
+                            self.selected_layers.add(name)
+                        else:
+                            # Solo remover si está visible y desmarcada
+                            self.selected_layers.discard(name)
+            
+            selectedServices = self.getSelectedItemsFromTable()
+            self.dlg.layer_name_box.clear()
+            if len(selectedServices) > 0:
+                names = ''
+                for selectedService in selectedServices:
+                    name_itr = selectedServices[selectedService].name
+                    names += name_itr + ','
+                names = names[:-1]
+                self.dlg.layer_name_box.setText(names)
+            else:
+                self.dlg.layer_name_box.clear()
+            self.dlg.layer_name_box.setReadOnly(True)
+        except Exception as e:
+            QgsMessageLog.logMessage("Error al actualizar capas seleccionadas: " + str(e))
+
+    def clearSelectedLayers(self):
+        """Limpia todas las capas seleccionadas"""
+        # Limpiar el conjunto de capas seleccionadas
+        self.selected_layers.clear()
+        
+        # Desmarcar todos los checkboxes visibles en la tabla
+        for row in range(self.dlg.table_widget.rowCount()):
+            checkbox_widget = self.dlg.table_widget.cellWidget(row, 0)
+            if checkbox_widget:
+                checkbox = checkbox_widget.findChild(QCheckBox)
+                if checkbox:
+                    checkbox.setChecked(False)
+        
+        # Limpiar el cuadro de texto
+        self.dlg.layer_name_box.clear()
 
     def loadWebService(self):
         # get selected items and add to the map
